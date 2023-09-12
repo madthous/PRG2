d7c0f213eb2559112c2e74f6aae2b4a9fe1e638c
madthous <matous.ferda@gmail.com>
2023-09-12 11:01:51 +0200

    prvykomit

---
+++ Prvyprojekt/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prvyprojekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Aká tvoja výškav centimetroch?");
+            int v = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Tvoja výška: " + v + "cm");*/
+            Console.WriteLine("Aký diamétr v cm?");
+            int r = Convert.ToInt32(Console.ReadLine());
+            double S = r * r * Math.PI;
+            double O = 2 * r * Math.PI;
+            Console.WriteLine("Plocha: " + S + "cm^2, Obvod: " + O + "cm");
+            Console.ReadKey();
+        }
+    }
+}
