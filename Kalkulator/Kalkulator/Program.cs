4a533e63585db4c36f653b508fecb527f66292df
madthous <matous.ferda@gmail.com>
2023-09-25 07:57:20 +0200

    dodatky na kalkulacku

--- Kalkulator/Kalkulator/Program.cs
+++ Kalkulator/Kalkulator/Program.cs
@@ -10,87 +10,134 @@ namespace Kalkulator
     {
         static void Main(string[] args)
         {
-            double res = 0;
-            double a = 0;
-            double b = 0;
-            bool o = true;
-            Console.WriteLine("Zadej numero uno/ základ mocniny či odmocniny:");
-            while (a == 0)
-                if (double.TryParse(Console.ReadLine(), out a))
+            double res;
+            double lastres = 0;
+            string input;
+            double a;
+            double b;
+            bool o;
+            bool p = true;
+            bool q;
+            Console.WriteLine("Zmáčkni jakoukoli klávesu pro kalkulátor");
+            while (p)
+            {
+                a = 0;
+                b = 0;
+                res = 0;
+                o = true;
+                Console.WriteLine();
+                Console.WriteLine("Zadej numero uno/základ");
+                while (a == 0)
                 {
-                    Console.WriteLine($"Povedlo se, hodnotoš: {a}");
-                }
-                else
-                {
-                    Console.WriteLine("Zkus to znova");
-                }
-
-            Console.WriteLine("Zadej numero dos/ mocninu či odmocninu:");
-            while (b == 0)
-                if (double.TryParse(Console.ReadLine(), out b))
-                {
-                    Console.WriteLine($"Povedlo se, hodnotoš: { b}");
-                }
-                else
-                {
-                    Console.WriteLine("Zkus to znova");
-                }
-            Console.WriteLine("Zadej název operace:");
-            Console.WriteLine("Máš na výběr z: součet, či + ; odečet, či - ; násobení, či * ; dělení, či / ; modulo, či % ; mocnina ; odmocnina");
-            while(o)
-                {
-                o = false;
-                string op = Console.ReadLine();
-                switch (op)
-                {
-                    case ("součet"):
-                        res = a + b;
-                        break;
-                    case ("+"):
-                        res = a + b;
-                        break;
-                    case ("odečet"):
-                        res = a - b;
-                        break;
-                    case ("-"):
-                        res = a - b;
-                        break;
-                    case ("násobení"):
-                        res = a * b;
-                        break;
-                    case ("*"):
-                        res = a * b;
-                        break;
-                    case ("dělení"):
-                        res = a / b;
-                        break;
-                    case ("/"):
-                        res = a / b;
-                        break;
-                    case ("modulo"):
-                        res = a % b;
-                        break;
-                    case ("%"):
-                        res = a % b;
-                        break;
-                    case ("mocnina"):
-                        res = Math.Pow(a, b);
-                        break;
-                    case ("odmocnina"):
-                        res = Math.Pow(a, 1/b);
-                        break;
-
-
-                    default:
+                    if (lastres != 0)
+                    {
+                        Console.WriteLine("Chceš zadat minulý výsledek?");
+                        input = Console.ReadLine();
+                        if (input != "ano" || input != "ne")
+                        {
+                            Console.WriteLine(" ano nebo ne");
+                        }
+                        else if (input == "ano")
+                        {
+                            a = lastres;
+                        }
+                        else
+                        {
+                            if (double.TryParse(Console.ReadLine(), out a))
+                            {
+                                Console.WriteLine($"Povedlo se, hodnotoš: {a}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Zkus to znova");
+                            }
+                        }
+                    }
+                    if (double.TryParse(Console.ReadLine(), out a))
+                    {
+                        Console.WriteLine($"Povedlo se, hodnotoš: {a}");
+                    }
+                    else
+                    {
                         Console.WriteLine("Zkus to znova");
-                        o = true;
-                        break;
+                    }
                 }
-                
+                    
+
+                Console.WriteLine("Zadej numero dos:");
+                while (b == 0)
+                    if (double.TryParse(Console.ReadLine(), out b))
+                    {
+                        Console.WriteLine($"Povedlo se, hodnotoš: {b}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Zkus to znova");
+                    }
+                Console.WriteLine("Zadej název operace:");
+                Console.WriteLine("Máš na výběr z: součet, či + ; odečet, či - ; násobení, či * ; dělení, či / ; modulo, či % ; mocnina ; odmocnina ; log ; ");
+                while (o)
+                {
+                    o = false;
+                    string op = Console.ReadLine();
+                    switch (op)
+                    {
+                        case ("součet"):
+                            res = a + b;
+                            break;
+                        case ("+"):
+                            res = a + b;
+                            break;
+                        case ("odečet"):
+                            res = a - b;
+                            break;
+                        case ("-"):
+                            res = a - b;
+                            break;
+                        case ("násobení"):
+                            res = a * b;
+                            break;
+                        case ("*"):
+                            res = a * b;
+                            break;
+                        case ("dělení"):
+                            res = a / b;
+                            break;
+                        case ("/"):
+                            res = a / b;
+                            break;
+                        case ("modulo"):
+                            res = a % b;
+                            break;
+                        case ("%"):
+                            res = a % b;
+                            break;
+                        case ("mocnina"):
+                            res = Math.Pow(a, b);
+                            break;
+                        case ("odmocnina"):
+                            res = Math.Pow(a, 1 / b);
+                            break;
+                        case ("log"):
+                            res = Math.Log(b, a);
+                            break;
+
+
+
+                        default:
+                            Console.WriteLine("Zkus to znova");
+                            o = true;
+                            break;
+                    }
+                    lastres = res;
+                    Console.WriteLine("Výsledek činí: " + res);
+                    if (Console.ReadKey().Key != ConsoleKey.Escape)
+                    {
+                        p = false;
+                    }
+                    
+                }
             }
-            
-            Console.WriteLine("Výsledek činí: " + res);
-            Console.ReadKey();
         }
     }
 }
