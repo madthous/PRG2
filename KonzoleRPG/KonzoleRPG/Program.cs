07ae94fe5e8fad09fdfb58631988621ba8a50df6
madthous <matous.ferda@gmail.com>
2024-01-09 11:21:15 +0100

    blemcht

---
+++ KonzoleRPG/KonzoleRPG/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonzoleRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vítej dobrodruhu:\n Právě ses narodil a je jen na tobě co se s tebou stane.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ KonzoleRPG/KonzoleRPG/Trouba.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonzoleRPG
+{
+    internal class Trouba
+    {
+        public int intelligence;
+        public int strength;
+        public int health;
+        public int defense;
+        public int speed;
+        public int speech;
+    }
+}
