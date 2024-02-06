6c8b6f58a4f7c32004b742a541a38e6ad56ca1bf
madthous <matous.ferda@gmail.com>
2024-02-06 11:22:32 +0100

    06_02_2024_Grafik

---
+++ Grafik/Grafik/Node.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Grafik
+{
+    //Simple node class, no comments needed
+    internal class Node
+    {
+        public int index;
+
+        public List<Node> neighbors;
+
+        public bool visited;
+
+        public Node cameFrom;
+
+        public Node(int index)
+        {
+            this.index = index;
+            neighbors = new List<Node>();
+            visited = false;
+            cameFrom = null;
+        }
+    }
+}
---
+++ Grafik/Grafik/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Grafik
+{
+    internal class Program
+    {
+        public static void DFS(Graph graph, Node startNode, Node targetNode = null)
+        {
+            Node currentNode = startNode;
+            while (true)
+            {
+                Node neighborToVisit = null;
+                foreach (Node neighbor in currentNode.neighbors)
+                {
+                    if (!neighbor.visited)
+                    {
+                        neighborToVisit = neighbor;
+                        break;
+                    }
+                }
+                if (neighborToVisit == null)
+                {
+                    if (currentNode == startNode)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.cameFrom;
+                    }
+                }
+                else
+                {
+                    neighborToVisit.visited = true;
+                    neighborToVisit.cameFrom = currentNode;
+                    currentNode = neighborToVisit;
+                }
+            }
+        }
+        public static void BFS(Graph graph, Node startNode, Node targetNode = null)
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            //Create and print the graph
+            Graph graph = new Graph();
+            graph.PrintGraph();
+            graph.PrintGraphForVisualization();
+
+            //Call both algorithms with a random starting node
+            Random rng = new Random();
+            DFS(graph, graph.nodes[rng.Next(0, graph.nodes.Count)]);
+            BFS(graph, graph.nodes[rng.Next(0, graph.nodes.Count)]);
+
+            Console.ReadKey();
+        }
+    }
+}
