47571586092161c4c4037b0cd4539b2154ee634d
madthous <matous.ferda@gmail.com>
2024-03-12 11:17:35 +0100

    12_3_2024

--- MSPaint/MSPaint/Form1.Designer.cs
+++ MSPaint/MSPaint/Form1.Designer.cs
@@ -29,9 +29,7 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.knedlikDel = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // panel1
@@ -41,41 +39,26 @@
             this.panel1.Size = new System.Drawing.Size(918, 657);
             this.panel1.TabIndex = 0;
             // 
-            // button1
+            // knedlikDel
             // 
-            this.button1.Location = new System.Drawing.Point(274, 68);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(78, 76);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "BRUSH";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(374, 68);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(78, 76);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "FILL";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(480, 68);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(78, 76);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "ERASE";
-            this.button3.UseVisualStyleBackColor = true;
+            this.knedlikDel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.knedlikDel.Appearance = System.Windows.Forms.Appearance.Button;
+            this.knedlikDel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.knedlikDel.Location = new System.Drawing.Point(235, 64);
+            this.knedlikDel.Name = "knedlikDel";
+            this.knedlikDel.Size = new System.Drawing.Size(104, 81);
+            this.knedlikDel.TabIndex = 1;
+            this.knedlikDel.TabStop = true;
+            this.knedlikDel.Text = "ERASE";
+            this.knedlikDel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.knedlikDel.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1434, 923);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.knedlikDel);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -86,9 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton knedlikDel;
     }
 }
 
