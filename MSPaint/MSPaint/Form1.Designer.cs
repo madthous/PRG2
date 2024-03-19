2bd4e3c35f2450c5f30390b3e28973381416a606
madthous <matous.ferda@gmail.com>
2024-03-19 11:15:43 +0100

    pokr_mspaint_19_3_2024

--- MSPaint/MSPaint/Form1.Designer.cs
+++ MSPaint/MSPaint/Form1.Designer.cs
@@ -30,14 +30,21 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.knedlikDel = new System.Windows.Forms.RadioButton();
+            this.knedlikPen = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Location = new System.Drawing.Point(235, 186);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(918, 657);
             this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
             // 
             // knedlikDel
             // 
@@ -52,12 +59,29 @@
             this.knedlikDel.Text = "ERASE";
             this.knedlikDel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.knedlikDel.UseVisualStyleBackColor = false;
+            this.knedlikDel.Click += new System.EventHandler(this.knedlikDel_Click);
+            // 
+            // knedlikPen
+            // 
+            this.knedlikPen.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.knedlikPen.Appearance = System.Windows.Forms.Appearance.Button;
+            this.knedlikPen.BackColor = System.Drawing.Color.Silver;
+            this.knedlikPen.Location = new System.Drawing.Point(345, 64);
+            this.knedlikPen.Name = "knedlikPen";
+            this.knedlikPen.Size = new System.Drawing.Size(104, 81);
+            this.knedlikPen.TabIndex = 2;
+            this.knedlikPen.TabStop = true;
+            this.knedlikPen.Text = "PEN";
+            this.knedlikPen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.knedlikPen.UseVisualStyleBackColor = false;
+            this.knedlikPen.Click += new System.EventHandler(this.knedlikPen_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1434, 923);
+            this.Controls.Add(this.knedlikPen);
             this.Controls.Add(this.knedlikDel);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
@@ -70,6 +94,7 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.RadioButton knedlikDel;
+        private System.Windows.Forms.RadioButton knedlikPen;
     }
 }
 
--- MSPaint/MSPaint/Form1.cs
+++ MSPaint/MSPaint/Form1.cs
@@ -12,9 +12,58 @@ namespace MSPaint
 {
     public partial class Form1 : Form
     {
+        Graphics g;
+        int x = -1;
+        int y = -1;
+        bool moving;
+        Pen pen;
         public Form1()
         {
             InitializeComponent();
+            g = panel1.CreateGraphics();
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            pen = new Pen(Color.Black, 4);
+            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            moving = true;
+            x = e.X; y = e.Y;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (moving && x != -1 && y != -1)
+            {
+                g.DrawLine(pen, new Point(x, y), e.Location);
+                x = e.X;
+                y = e.Y;
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            moving = false;
+            x = -1;
+            y = -1;
+        }
+
+        private void knedlikPen_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Black;
+            pen.Width = 4;
+        }
+
+        private void knedlikDel_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.White;
+            pen.Width = 25;
         }
     }
 }
